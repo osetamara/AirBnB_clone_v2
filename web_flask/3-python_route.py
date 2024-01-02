a189459759f68754904e577816a9abcac29e643f
osetamara <osemudiamebrifini@gmail.com>
2024-01-02 08:32:39 -0800

    _list.py

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -8,16 +9,22 @@ app = Flask(__name__)
 def hello_holberton():
     """Returns a string at the root route"""
     return 'Hello HBNB!'
+
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Returns a string at the /hbnb route"""
     return 'HBNB'
+
+
 @app.route('/c/<text>', strict_slashes=False)
 def cisfun(text):
     """Returns a string at the /c/<text> route,
     expands the <text> variable"""
     new = text.replace('_', ' ')
     return 'C %s' % new
+
+
 @app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def pythoniscool(text):
