50870bb44b6c966ee3da012cacc10d4d4758831d
osetamara <osemudiamebrifini@gmail.com>
2024-01-02 08:39:20 -0800

    _list.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-"""Start a Flask web app.
-This script defines a simple Flask web application that responds with
-'Hello HBNB!' when the root route ('/') is queried.
-Usage:
-    - Run the script to start the Flask app on '0.0.0.0' and port 5000.
-Note:
-    - The `app.url_map.strict_slashes` is set to False to allow both
-      '/path' and '/path/' to be valid routes.
-Dependencies:
-    - Flask library is required.
-Author:
-    [Your Name]
-"""
+"""Starts a Flask web application"""
+
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello_flask():
-    """Return string when route is queried."""
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at the root route"""
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
-"""Simple Flask app with two routes: '/' and '/hbnb'."""
+"""Starts a Flask web application"""
+
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    """Return 'Hello HBNB!' for the root route."""
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at the root route"""
     return 'Hello HBNB!'
-@app.route('/hbnb')
+
+
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return 'HBNB' for the '/hbnb' route."""
+    """Returns a string at the /hbnb route"""
     return 'HBNB'
+
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0')
