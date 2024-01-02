9dc20916c1248ff88cd21b6a696bcc30e0f6b5cc
osetamara <osemudiamebrifini@gmail.com>
2024-01-02 08:28:51 -0800

    _list.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-"""Simple Flask app with three routes: '/', '/hbnb', and '/c/<text>'."""
+"""Simple Flask app with three routes:"""
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-    """Return 'Hello HBNB!' for the root route."""
+    """Return 'Hello HBNB!' for root route."""
     return 'Hello HBNB!'
 @app.route('/hbnb')
 def hbnb():
-    """Return 'HBNB' for the '/hbnb' route."""
+    """Return 'HBNB' for'/hbnb' route."""
     return 'HBNB'
 @app.route('/c/<text>')
 def c_is_fun(text):
-    """Return reformatted text with 'C ' prefix."""
+    """Return reformatted text with 'C 'prefix."""
     return 'C ' + text.replace('_', ' ')
 
 if __name__ == '__main__':
--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,30 +1,31 @@
 #!/usr/bin/python3
-"""Simple Flask app with multiple routes: 
-'/', '/hbnb', '/c/<text>', and '/python/' or '/python/<text>'.
-"""
+"""Starts a Flask web application"""
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    """Return 'Hello HBNB!' for the root route."""
+
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at the root route"""
     return 'Hello HBNB!'
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Return 'HBNB' for the '/hbnb' route."""
+    """Returns a string at the /hbnb route"""
     return 'HBNB'
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text with 'C ' prefix."""
-    return 'C ' + text.replace('_', ' ')
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Return reformatted text with 'Python
-    ' prefix based on optional variable.
-    """
-    return 'Python ' + text.replace('_', ' ')
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Returns a string at the /c/<text> route,
+    expands the <text> variable"""
+    new = text.replace('_', ' ')
+    return 'C %s' % new
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """Returns a string at the /python route, with a default text
+    of 'is cool', or the expansion of <text>"""
+    new = text.replace('_', ' ')
+    return 'Python %s' % new
+
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0')
