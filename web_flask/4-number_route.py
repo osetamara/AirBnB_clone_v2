564dd472f1dbf31dd56e2d1ba4f3e3b00feb72a6
osetamara <osemudiamebrifini@gmail.com>
2024-01-02 08:24:03 -0800

    _list.py

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,29 +1,45 @@
 #!/usr/bin/python3
-"""Simple Flask app with multiple routes including dynamic '/number/<int:n>'."""
+"""Starts a Flask web application"""
+
 from flask import Flask
 app = Flask(__name__)
-@app.route('/')
-def hello():
-    """Return 'Hello HBNB!' for the root route."""
-    return 'Hello HBNB!'
-@app.route('/hbnb')
-def hbnb():
-    """Return 'HBNB' for the '/hbnb' route."""
-    return 'HBNB'
-@app.route('/c/<text>')
-def c_is_fun(text):
-    """Return reformatted text with 'C ' prefix."""
-    return 'C ' + text.replace('_', ' ')
-@app.route('/python/')
-@app.route('/python/<text>')
-def python_with_text(text='is cool'):
-    """Return reformatted text with 'Python ' prefix based on optional variable."""
-    return 'Python ' + text.replace('_', ' ')
-@app.route('/number/<int:n>')
-def number(n=None):
-    """Allow request if path variable is a valid integer and return it as a string."""
-    return str(n) + ' is a number'
-if __name__ == '__main__':
-    app.url_map.strict_slashes = False
-    app.run(host='0.0.0.0', port=5000)
 
+
+@app.route('/', strict_slashes=False)
+def hello_holberton():
+    """Returns a string at the root route"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns a string at the /hbnb route"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def cisfun(text):
+    """Returns a string at the /c/<text> route,
+    expands the <text> variable"""
+    new = text.replace('_', ' ')
+    return 'C %s' % new
+
+
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def pythoniscool(text):
+    """Returns a string at the /python route, with a default text
+    of 'is cool', or the expansion of <text>"""
+    new = text.replace('_', ' ')
+    return 'Python %s' % new
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """Returns a string at the /number/<n> route,
+    only if n is an int"""
+    if type(n) == int:
+        return '%i is a number' % n
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
